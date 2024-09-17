c4d2d1451f36dcefd4eecd1db7a5e472a00fcedd
Joseph Richardson <rijoseph@umich.edu>
2024-09-17 13:36:17 -0400

    Revert ":construction: Fix relative URLs while it is on github pages"
    This reverts commit b8060b930c7bd539575fe0017d8cbc392bdd5ff2.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,4 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({
-    site: 'https://its-admin-dux.github.io',
-    base: '/its-netbasilisk'
-});
+export default defineConfig({});
